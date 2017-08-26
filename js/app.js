0fb926cab7efd64d61bc3c8336196ab17727dadd
Daniel Tofan <daniel@ideaedu.org>
2017-08-26 18:31:01 -0400

    Implemented progress bars for each question section and populated them dynamically. Implemented an overall progress bar for the entire form.

--- js/app.js
+++ js/app.js
@@ -1,19 +1,20 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var allSectionCounts = {part1a: 0, part1b: 0, part2: 0, part3: 0, part4: 0};
+var totalQuestions = 26;
+
+var globalTheme;
 
 $(document).ready(function () {
     /* On load, set theme from local storage */
     if (supports_storage) {
         var theme = localStorage.theme;
+        var theme_name = localStorage.theme_name;
+        
         if (theme) {
-            set_theme(theme);
+            set_theme(theme, theme_name);
         }
     } else {
         /* Don't annoy user with options that don't persist */
-        $('#theme-dropdown').hide();
+        //$('#theme-dropdown').hide();
     }
 
     $('body').on('click', '.change-style-menu-item', function () {
@@ -21,7 +22,52 @@ $(document).ready(function () {
         var theme = "//netdna.bootstrapcdn.com/bootswatch/3.3.7/" + theme_name + "/bootstrap.min.css";
         set_theme(theme, theme_name);
     });
+    
+    $('.btn-group input[type=radio]').on('change', function() {
+       updateProgress($(this));
+    });
+    
+    $('.form-control').on('change', function() {
+        var parent = $(this).closest('.form-control-parent');
+        var hasButtonActive = $(parent).has('.btn.active').length;
+        if (hasButtonActive === 0) $(parent).append('<button class="btn active" style="display: none">a</button>');
+        updateProgress($(this));
+    });
 });
+
+function updateProgress(input)
+{
+    var section = input.closest('div').closest('div.question-section');
+    var sectionName = section.attr('id');
+
+    var checkCount = $(section).find('.btn.active').length;
+
+    var qCount = $(section).find('li').length;
+
+    var progress = $('div#' + sectionName + '-panel').find('span.progress');
+    var bar = $(progress).find('span.progress-bar');
+
+    $(progress).css('opacity', 0.8);
+    var prog = checkCount * 100.0 / qCount;
+    $(bar).css('width', prog + '%');
+    $(bar).text(checkCount + ' / ' + qCount);
+    
+    allSectionCounts[sectionName] = checkCount;
+    prog = getCompletedQuestionCount() * 100 / totalQuestions;
+    
+    var overallProgress = $('#overallProgress');
+    console.log(overallProgress);
+    $(overallProgress).css('width', prog + '%');
+    
+    if (prog >= 75.0)
+    {
+        $(overallProgress).removeClass('progress-bar-warning').addClass('progress-bar-success');
+    }
+    else if (prog >= 30.0)
+    {
+        $(overallProgress).removeClass('progress-bar-danger').addClass('progress-bar-warning');
+    }
+}
 
 function set_theme(theme, theme_name) {
     $('link[title="main"]').attr('href', theme);
@@ -30,7 +76,14 @@ function set_theme(theme, theme_name) {
     
     if (supports_storage) {
         localStorage.theme = theme;
+        localStorage.theme_name = theme_name;
     }
+    
+    globalTheme = theme_name || 'darkly';
+    console.log(globalTheme);
+    
+    var pt = getProgressPaddingTop();
+    $('span.progress-bar').css('padding-top', pt + 'px');
 }
 
 function supports_html5_storage() {
@@ -42,3 +95,18 @@ function supports_html5_storage() {
 }
 
 var supports_storage = supports_html5_storage();
+
+var themePaddings = 
+    {
+        'darkly': 4, cyborg: 0, slate: 0, solar: 0, superhero: 0, cosmo: 4, flatly: 4, lumen: 0, paper: 0, spacelab: 0, united: 0
+    };
+    
+function getProgressPaddingTop()
+{
+    return themePaddings[globalTheme];
+}
+
+function getCompletedQuestionCount()
+{
+    return allSectionCounts.part1a + allSectionCounts.part1b + allSectionCounts.part2 + allSectionCounts.part3 + allSectionCounts.part4;
+}
